44f9c6386ea0b3864ec04b63226e66b365af2c15
Nelio Alves <acenelio@gmail.com>
2018-04-21 16:41:19 -0300

    Implementing possible moves of King

--- Chess/Pieces/King.cs
+++ Chess/Pieces/King.cs
@@ -6,8 +6,58 @@ namespace Chess.Pieces {
         public King(Board board, Color color) : base(board, color) {
         }
 
+        private bool CanMove(Position position) {
+            ChessPiece p = (ChessPiece)Board.Piece(position);
+            return p == null || p.Color != Color;
+        }
+
         public override bool[,] PossibleMoves() {
-            return new bool[Board.Rows, Board.Columns];
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position p = new Position(0, 0);
+
+            // above
+            p.setValues(Position.Row - 1, Position.Column);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // ne
+            p.setValues(Position.Row - 1, Position.Column + 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // right
+            p.setValues(Position.Row, Position.Column + 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // se
+            p.setValues(Position.Row + 1, Position.Column + 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // below
+            p.setValues(Position.Row + 1, Position.Column);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // sw
+            p.setValues(Position.Row + 1, Position.Column - 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // left
+            p.setValues(Position.Row, Position.Column - 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+            // nw
+            p.setValues(Position.Row - 1, Position.Column - 1);
+            if (Board.PositionExists(p) && CanMove(p)) {
+                mat[p.Row, p.Column] = true;
+            }
+
+            return mat;
         }
 
         public override string ToString() {
